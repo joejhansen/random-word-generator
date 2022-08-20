acb6f81766e2fed73823c9038ce66e525db11fe5
joejhansen <josephjameshansen@gmail.com>
2022-08-20 10:16:30 -0700

    initial commit of files

---
+++ assets/javascript/script.js
@@ -0,0 +1,118 @@
+const formGroupEl = document.getElementById("formGroup")
+const historyUl = document.getElementById("historyUl")
+const wordDisplay = document.getElementById("wordDisplay")
+const definitionDiv = document.getElementById("definitionDiv")
+const generateBtn = document.getElementById("generateBtn")
+var wordList = ["theater", "doctor", "park", "hospital"]
+var apiURL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+function addWordToList(event) {
+    event.preventDefault()
+    // console.log(event.target.children[1].value)
+    var thisWord = event.target.children[1].value
+    fetch(apiURL + thisWord)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json()
+                    .then(function (data) {
+                        // console.log(data)
+                        var partOfSpeech = data[0].meanings[0].partOfSpeech
+                        var definition = data[0].meanings[0].definitions[0].definition
+                        wordDisplay.textContent = thisWord
+                        definitionDiv.innerHTML = "<p>" + partOfSpeech + "</p><p>" + definition + "</p>"
+                        if (wordList.includes(thisWord)) {
+                            return
+                        } else {
+                            wordList.push(thisWord)
+                            var saveThis = JSON.stringify(wordList)
+                            localStorage.setItem("wordsList", saveThis)
+                        }
+                        var newLi = document.createElement("li")
+                        newLi.textContent = thisWord
+                        historyUl.appendChild(newLi)
+                        // console.log(wordList)
+                    });
+
+            } else {
+                console.log("error")
+                return
+            }
+        })
+    event.target.children[1].value = ''
+}
+
+formGroupEl.addEventListener("submit", addWordToList)
+
+function showWordDefinition(event) {
+    var thisWord = event.target.textContent
+    fetch(apiURL + thisWord)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json()
+                    .then(function (data) {
+                        // console.log(data)
+                        var partOfSpeech = data[0].meanings[0].partOfSpeech
+                        var definition = data[0].meanings[0].definitions[0].definition
+                        wordDisplay.textContent = thisWord
+                        definitionDiv.innerHTML = "<p>" + partOfSpeech + "</p><p>" + definition + "</p>"
+                        if (wordList.includes(thisWord)) {
+                            return
+                        } else {
+                            wordList.push(thisWord)
+                            var saveThis = JSON.stringify(wordList)
+                            localStorage.setItem("wordsList", saveThis)
+                        }
+                        // console.log(wordList)
+                    });
+
+            } else {
+                console.log("error")
+                return
+            }
+        })
+    console.log(event.target.textContent)
+}
+
+historyUl.addEventListener("click", showWordDefinition)
+
+function showRandomWord() {
+    var thisWord = wordList[Math.floor(Math.random() * wordList.length)]
+    fetch(apiURL + thisWord)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json()
+                    .then(function (data) {
+                        // console.log(data)
+                        var partOfSpeech = data[0].meanings[0].partOfSpeech
+                        var definition = data[0].meanings[0].definitions[0].definition
+                        wordDisplay.textContent = thisWord
+                        definitionDiv.innerHTML = "<p>" + partOfSpeech + "</p><p>" + definition + "</p>"
+                        // console.log(wordList)
+                    });
+
+            } else {
+                console.log("error")
+                return
+            }
+        })
+}
+
+generateBtn.addEventListener("click", showRandomWord)
+
+function init() {
+    var localWords = localStorage.getItem("wordsList")
+    if (!localWords) {
+        var saveThis = JSON.stringify(wordList)
+        localStorage.setItem("wordsList", saveThis)
+        localWords = localStorage.getItem("wordsList")
+    } else {
+        wordList = JSON.parse(localWords)
+    }
+    wordList.forEach(function (number) {
+        var newLi = document.createElement("li")
+        newLi.textContent = number
+        historyUl.appendChild(newLi)
+    })
+}
+
+init()
